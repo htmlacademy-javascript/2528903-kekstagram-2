e6f113bc6132b9eb82aa18e1e15e6c11c25e7285
Avaqulon <feadosy24@gmail.com>
2025-04-17 22:12:53 +0300

    1 задача

---
+++ js/functions.js
@@ -0,0 +1,14 @@
+const checkLenght = (string, lenght) => {
+  let result = string <= lenght;
+
+  return result;
+
+  /*   if (string <= lenght){
+    return true;
+  }
+  return false; */
+}
+
+// Строка меньше указанной длины - true
+
+// Строка больше указанной длины - false
