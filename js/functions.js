f9420fe235a7146c1daae2b235361e2593b90d64
Avaqulon <feadosy24@gmail.com>
2025-04-18 14:30:33 +0300

    Длина строки + палиндром js

--- js/functions.js
+++ js/functions.js
@@ -1,14 +1,20 @@
+// Проверка длины строки
 const checkLenght = (string, lenght) => {
-  let result = string <= lenght;
-
+  const result = (string <= lenght);
   return result;
-
   /*   if (string <= lenght){
     return true;
   }
   return false; */
 };
 
-// Строка меньше указанной длины - true
-
-// Строка больше указанной длины - false
+// Является ли строка палиндромом
+const isPalindrome = (string) => {
+  const normalizedString = string.replaceAll(' ', '').toLowerCase();
+  let emptyString = '';
+  for (let i = normalizedString.length - 1; i >= 0; i--) {
+    const symbol = normalizedString[i];
+    emptyString += symbol;
+  }
+  return emptyString === normalizedString;
+};
