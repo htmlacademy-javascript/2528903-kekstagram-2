21f551d16eeeb6313d20c321b9a79523f35308d3
Avaqulon <feadosy24@gmail.com>
2025-04-24 17:05:27 +0300

    Доделал задачи 2-ой темы

--- js/functions.js
+++ js/functions.js
@@ -1,21 +1,14 @@
 // Проверка длины строки
-const checkLength = (string, length) => {
-  const result = (string <= length);
-  return result;
-  /*   if (string <= length){
-    return true;
-  }
-  return false; */
-};
+const checkLength = (string, length) => string.length <= length;
+checkLength();
 
 // Является ли строка палиндромом
 const isPalindrome = (string) => {
   const normalizedString = string.replaceAll(' ', '').toLowerCase();
   let emptyString = '';
   for (let i = normalizedString.length - 1; i >= 0; i--) {
-    /* const symbol = normalizedString[i];
-    emptyString += symbol; */
     emptyString += normalizedString[i];
   }
   return emptyString === normalizedString;
 };
+isPalindrome();
